cdea7a899f103fc42a48ad7a318844bb1e6ffaf3
linyuanchuang <767135340@qq.com>
2022-09-12 20:26:51 +0800

    test html

--- test/js/index.js
+++ test/js/index.js
@@ -1,3 +1,4 @@
 const p = document.querySelector('p');
 // p.style.color = 'blue'
-p.className += ' p-blue';
+// p.className += ' p-blue';
+document.styleSheets[0].insertRule('.blue { color: gray }', 0);
