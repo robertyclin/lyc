bb87d27a33d79bc3fb943d4a778018de91c163fa
linyuanchuang <767135340@qq.com>
2023-06-16 10:50:59 +0800

    test

--- vue-project/vite.config.ts
+++ vue-project/vite.config.ts
@@ -7,6 +7,9 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
+  build: {
+    target: ['es2015']
+  },
   plugins: [
     vue(),
     vueJsx(),
