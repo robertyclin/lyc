00628e5bbfed8dac133d38b921128683e4858c2e
linyuanchuang <767135340@qq.com>
2023-06-22 10:15:37 +0800

    test

--- vue-project/vite.config.ts
+++ vue-project/vite.config.ts
@@ -7,7 +7,7 @@ import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/lyc/vue-project',
   // build: {
   //   target: ['es2015']
   // },
